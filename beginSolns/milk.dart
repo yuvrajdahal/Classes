930853b13353b1fd79cbc9847be5ee6cea6dc5dc
PrijeshBikramShahi <prijeshshahi0@gmail.com>
2022-07-30 14:44:02 +0545

    prices found

---
+++ beginSolns/milk.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+main() {
+  const pi = 3.14;
+  double heightOfCylinder = 50;
+  double radiusOfCylinder = 40;
+  double volumeOfCylinder =
+      pi * radiusOfCylinder * radiusOfCylinder * heightOfCylinder;
+  print("the volume of cylinder is $volumeOfCylinder");
+  print(
+      "the price of milk per litre is 40rs and per litre consumes about 1000cm of volume then");
+  double amountToPay = volumeOfCylinder / 1000;
+  print("the amount to be paid is $amountToPay rs");
+}
