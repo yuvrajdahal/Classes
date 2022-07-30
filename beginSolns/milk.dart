556ac829b6f0ea5621dfd181f7acf868d16dd41d
PrijeshBikramShahi <prijeshshahi0@gmail.com>
2022-07-30 14:53:13 +0545

    decimals fixed

--- beginSolns/milk.dart
+++ beginSolns/milk.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 main() {
-  const pi = 3.14;
   double heightOfCylinder = 50;
   double radiusOfCylinder = 40;
   double volumeOfCylinder =
@@ -10,5 +9,6 @@ main() {
   print(
       "the price of milk per litre is 40rs and per litre consumes about 1000cm of volume then");
   double amountToPay = volumeOfCylinder / 1000;
-  print("the amount to be paid is $amountToPay" + "rs");
+  double amountInDecimal = double.parse(amountToPay.toStringAsFixed(2));
+  print("the amount to be paid is $amountInDecimal" + "rs");
 }
