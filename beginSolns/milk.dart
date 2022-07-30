ffa630d60bd15f759acae84bc6d349ca1857d51f
PrijeshBikramShahi <prijeshshahi0@gmail.com>
2022-07-30 14:58:16 +0545

    acp commit

--- beginSolns/milk.dart
+++ beginSolns/milk.dart
@@ -7,7 +7,7 @@ main() {
       pi * radiusOfCylinder * radiusOfCylinder * heightOfCylinder;
   print("the volume of cylinder is $volumeOfCylinder");
   print(
-      "the price of milk per litre is 40rs and per litre consumes about 1000cm of volume then");
+      "the price of milk per litre is 40rs and per litre consumes about 1000cm^3 of volume then");
   double amountToPay = volumeOfCylinder / 1000;
   double amountInDecimal = double.parse(amountToPay.toStringAsFixed(2));
   print("the amount to be paid is $amountInDecimal" + "rs");
